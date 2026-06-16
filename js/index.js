ca2cc37b227c2c62d838c3a09e1b82992cfa459d
congu <connerguillerman@gmail.com>
2026-06-16 18:01:36 -0500

    Fix case-top height and delete black bars

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const caseTop = document.getElementById('caseTop');
     const powerButton = document.getElementById('powerButton');
+
+    setSize(caseTop, "100%")
+    setPosition(caseTop, "50%", "50%")
     
 });
