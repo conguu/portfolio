92dd96293886e3be5318a9191689cb990b762dc7
congu <connerguillerman@gmail.com>
2026-06-17 09:29:23 -0500

    Add clickable button

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const caseTop = document.getElementById('caseTop');
     const powerButton = document.getElementById('powerButton');
 
-    setSize(caseTop, "100%")
-    setPosition(caseTop, "50%", "50%")
-    
+    setSize(caseTop, "100%");
+    setPosition(caseTop, "50%", "50%");
+
+    setSize(powerButton, "7vw");
+    setPosition(powerButton, "24.8%", "47.9%");
+
+    powerButton.addEventListener("click", function(e) {
+        
+    });
 });
